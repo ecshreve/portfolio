5ba65893b600601c482915613d971cd9bf934a8c
Eric Shreve <ecshreve13@gmail.com>
2021-04-05 19:42:28 -0700

    add resume download button

--- client/src/components/landing/LandingHeader.tsx
+++ client/src/components/landing/LandingHeader.tsx
@@ -1,77 +1,101 @@
-import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { Container, IconButton } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
-import GitHubIcon from '@material-ui/icons/GitHub'
-import LinkedIn from '@material-ui/icons/LinkedIn'
-import Email from '@material-ui/icons/Email'
+import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { Container, IconButton } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedIn from "@material-ui/icons/LinkedIn";
+import Email from "@material-ui/icons/Email";
+import GetApp from "@material-ui/icons/GetApp";
 
-import headshot from '../../img/headshot.jpeg'
-import CustomDivider from '../custom-divider/CustomDivider';
+import headshot from "../../img/headshot.jpeg";
+import CustomDivider from "../custom-divider/CustomDivider";
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-      fontSize: "large",
-      paddingTop: "150px",
-      paddingBottom: "175px",
-      backgroundColor: theme.palette.primary.light,
-    },
-    headshot: {
-      marginBottom: "25px",
-      borderRadius: "50%",
-      borderColor: "#ffffff",
-      borderStyle: "solid",
-      width: "15rem",
-      "box-shadow": "5px 5px 15px 10px rgba(0,0,0,0.5)",
-    },
-    name: {
-      flexGrow: 1,
-      fontWeight: "bold",
-    },
-    icons: {
-      color: "white",
-      fontSize: "large",
-      width: "2rem",
-      height: "2rem",
-    },
-    iconButton: {
-      borderRadius: "100%",
-      borderColor: "white",
-      border: "2px solid",
-      margin: "5px",
-      "&:hover": {
-        backgroundColor: theme.palette.primary.dark,
-        "box-shadow": "10px 10px 29px 0px rgba(0,0,0,0.8)",
-      },
-    },
-  }),
+	createStyles({
+		root: {
+			flexGrow: 1,
+			fontSize: "large",
+			paddingTop: "150px",
+			paddingBottom: "175px",
+			backgroundColor: theme.palette.primary.light,
+		},
+		headshot: {
+			marginBottom: "25px",
+			borderRadius: "50%",
+			borderColor: "#ffffff",
+			borderStyle: "solid",
+			width: "15rem",
+			"box-shadow": "5px 5px 15px 10px rgba(0,0,0,0.5)",
+		},
+		name: {
+			flexGrow: 1,
+			fontWeight: "bold",
+		},
+		icons: {
+			color: "white",
+			fontSize: "large",
+			width: "2rem",
+			height: "2rem",
+		},
+		iconButton: {
+			borderRadius: "100%",
+			borderColor: "white",
+			border: "2px solid",
+			margin: "5px",
+			"&:hover": {
+				backgroundColor: theme.palette.primary.dark,
+				"box-shadow": "10px 10px 29px 0px rgba(0,0,0,0.8)",
+			},
+		},
+	})
 );
 
 export default function LandingHeader() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <Container>
-        <img className={classes.headshot} src={headshot} alt=""></img>
-        <Typography className={classes.name} variant="h2">
-          Eric Shreve
-        </Typography>
-        <CustomDivider />
-        <div>
-          <IconButton className={classes.iconButton} href="https://github.com/ecshreve" target="_blank">
-            <GitHubIcon className={classes.icons} />
-          </IconButton>
-          <IconButton className={classes.iconButton} href="https://www.linkedin.com/in/ecshreve/" target="_blank">
-            <LinkedIn className={classes.icons} />
-          </IconButton>
-          <IconButton className={classes.iconButton} href="mailto:eric@shreve.dev" target="_blank">
-            <Email className={classes.icons} />
-          </IconButton>
-        </div>
-      </Container>
-    </div>
-  );
+	return (
+		<div className={classes.root}>
+			<Container>
+				<img className={classes.headshot} src={headshot} alt=""></img>
+				<Typography className={classes.name} variant="h2">
+					Eric Shreve
+				</Typography>
+				<CustomDivider />
+				<div>
+					<IconButton
+						className={classes.iconButton}
+						title="check out my github"
+						href="https://github.com/ecshreve"
+						target="_blank"
+					>
+						<GitHubIcon className={classes.icons} />
+					</IconButton>
+					<IconButton
+						className={classes.iconButton}
+						title="see my linkedin"
+						href="https://www.linkedin.com/in/ecshreve/"
+						target="_blank"
+					>
+						<LinkedIn className={classes.icons} />
+					</IconButton>
+					<IconButton
+						className={classes.iconButton}
+						title="send me an email"
+						href="mailto:eric@shreve.dev"
+						target="_blank"
+					>
+						<Email className={classes.icons} />
+					</IconButton>
+					<IconButton
+						className={classes.iconButton}
+						title="download my resume"
+						href="../static/files/EricShreveResume.pdf"
+						target="_blank"
+					>
+						<GetApp className={classes.icons} />
+					</IconButton>
+				</div>
+			</Container>
+		</div>
+	);
 }
